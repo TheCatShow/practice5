fd24e8ee5420d8195ba1bfa54329212503ffa97d
TheCatShow <75573235+TheCatShow@users.noreply.github.com>
2022-10-14 22:25:11 +0300

    Add files via upload

---
+++ practice5/script.js
@@ -0,0 +1,58 @@
+console.log ("*******Zadanie 1*******");
+
+const num=1;
+let result = num === 1 ? 'Один' : 'Один';
+console.log(result);
+
+console.log ("*******Zadanie 2*******");
+
+let result2 = 1 > 0;
+console.log (result2);
+
+console.log ("*******Zadanie 3*******");
+
+let m = 10;
+let n;
+if (m > 50) {
+    n = "большое";
+}
+else {
+    n = "маленькое";
+}
+console.log (n);
+
+console.log ("*******Zadanie 4*******");
+
+if (1,2,3) {
+    console.log (1,2,3);
+} else if (any) {
+    console.log ('Неизвестное значение');
+}
+
+console.log ("*******Zadanie 5*******");
+
+let i = 45;
+while (i < 68) {
+    console.log (i);
+    i++;
+}
+
+console.log ("*******Zadanie 6*******");
+
+let i2 = 45;
+while (i++ < 670) {
+    console.log (i2);
+}
+
+console.log ("*******Zadanie 7*******");
+
+for (let i = 45; i < 68; i++) {
+    console.log (i);
+}
+
+for (let i2 = 45; i++ < 670;) {
+    console.log (i2);
+}
+
+console.log ("*******Zadanie 8*******");
+
